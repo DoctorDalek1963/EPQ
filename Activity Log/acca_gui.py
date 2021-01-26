2f69ac6bc9a890ef27f28ee69e418a16970b3a9b
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2021-01-26 19:13:35 +0000

    Create GUI for inputting activities in the log

---
+++ Activity Log/acca_gui.py
@@ -0,0 +1,41 @@
+import tkinter as tk
+import _tkinter
+import acca_lib
+
+entryText = ''
+
+root = tk.Tk()
+root.title('Activity Logger')
+root.resizable(False, False)
+
+
+def write_entry():
+    acca_lib.write_entry(entryText)
+    text_box.delete(1.0, tk.END)
+
+
+text_box = tk.Text(root)
+write_button = tk.Button(root, text='Write entry to file', command=write_entry, state='disabled')
+
+text_box.grid(column=0, row=0, pady=(10, 30), padx=10)
+write_button.grid(column=0, row=1, pady=(0, 10))
+
+
+def update_loop():
+    global entryText
+
+    while True:
+        try:
+            entryText = text_box.get(1.0, tk.END)
+            if entryText != '\n':
+                write_button.config(state='normal')
+            else:
+                write_button.config(state='disabled')
+
+            root.update()
+        except _tkinter.TclError:
+            return
+
+
+if __name__ == '__main__':
+    update_loop()

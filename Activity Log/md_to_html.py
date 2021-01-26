e1fa058f18dab76c26cdbb644704837f5d299a74
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2021-01-26 17:32:34 +0000

    Create md_to_html.py

---
+++ Activity Log/md_to_html.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+
+def ordinal_day(date_time: datetime) -> str:
+    """Take a datetime object and return an ordinal day ('23rd' or '12th', for example)."""
+    day = str(date_time.day)
+
+    # Get day of month extension
+    if day.endswith('1') and day != '11':
+        extension = 'st'
+    elif day.endswith('2') and day != '12':
+        extension = 'nd'
+    elif day.endswith('3') and day != '13':
+        extension = 'rd'
+    else:
+        extension = 'th'
+
+    return day + extension
+
+
+class Entry:
+    def __init__(self, body_text: str):
+        self.body_text = body_text
+
+        now = datetime.now()
+
+        day_of_month = ordinal_day(now)
+
+        original_hour = now.hour
+        hour = original_hour if original_hour < 12 else original_hour - 12  # Convert to 12 hour
+
+        self.date_and_time = now.strftime(f'%A {day_of_month} %B %Y, {hour}:%M %p')

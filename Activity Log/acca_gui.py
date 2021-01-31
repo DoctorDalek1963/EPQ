26a2c391cda21e8621f699ee2e516c943a26840d
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2021-01-31 14:45:16 +0000

    Update info box and allow user to open link to markdownguide.org

--- Activity Log/acca_gui.py
+++ Activity Log/acca_gui.py
@@ -1,12 +1,18 @@
 import tkinter as tk
 import _tkinter
+import webbrowser
 import acca_lib
 
 entryText = ''
+markdown_link = 'https://www.markdownguide.org/basic-syntax/'
 
 root = tk.Tk()
 root.title('Activity Logger')
 root.resizable(False, False)
+
+
+def open_markdown_link(event):  # Entry is passed here because tkinter will throw an error if it isn't. The value isn't used
+    webbrowser.open_new(markdown_link)
 
 
 def write_entry():
@@ -14,13 +20,18 @@ def write_entry():
     text_box.delete(1.0, tk.END)
 
 
-info = tk.Label(root, text='This text box supports markdown formatting. For instance, you can do *italics*, **bold**, `code`, [links](https://links.com), etc.')
+info = tk.Label(root, text='This text box supports markdown formatting. For instance, you can do *italics*, **bold text**, \n'
+                           '`inline code`, [links](https://google.com), ![images](images/example.png), etc.\n\n')
+link = tk.Label(root, text=f'[Click this]({markdown_link}) to see everything you can do with markdown.')
 text_box = tk.Text(root, wrap='word')
 write_button = tk.Button(root, text='Write entry to file', command=write_entry, state='disabled')
 
 info.grid(column=0, row=0, pady=(10, 0), padx=10)
-text_box.grid(column=0, row=1, pady=(10, 30), padx=10)
-write_button.grid(column=0, row=2, pady=(0, 10))
+link.grid(column=0, row=1, pady=0, padx=10)
+text_box.grid(column=0, row=2, pady=(10, 30), padx=10)
+write_button.grid(column=0, row=3, pady=(0, 10))
+
+link.bind('<Button-1>', open_markdown_link)
 
 
 def update_loop():

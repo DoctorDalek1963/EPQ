b1bd42707500e61e35d5cb388b8433b88028c94a
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2021-02-06 15:51:45 +0000

    Add exit button to Activity Logger GUI

--- Activity Log/acca_gui.py
+++ Activity Log/acca_gui.py
@@ -21,11 +21,13 @@ info = tk.Label(root, text='This text box supports markdown formatting. For inst
 link = tk.Label(root, text=f'[Click this]({markdown_link}) to see everything you can do with markdown.')
 text_box = tk.Text(root, wrap='word')
 write_button = tk.Button(root, text='Write entry to file', command=write_entry, state='disabled')
+exit_button = tk.Button(root, text='Exit', command=root.destroy)
 
-info.grid(column=0, row=0, pady=(10, 0), padx=10)
-link.grid(column=0, row=1, pady=0, padx=10)
-text_box.grid(column=0, row=2, pady=(10, 30), padx=10)
-write_button.grid(column=0, row=3, pady=(0, 10))
+info.grid(column=0, row=0, pady=(10, 0), padx=10, columnspan=2)
+link.grid(column=0, row=1, pady=0, padx=10, columnspan=2)
+text_box.grid(column=0, row=2, pady=(10, 30), padx=10, columnspan=2)
+write_button.grid(column=0, row=3, pady=(0, 10), padx=(0, 20))
+exit_button.grid(column=1, row=3, pady=(0, 10), padx=(20, 0))
 
 link.bind('<Button-1>', lambda _: webbrowser.open_new(markdown_link))  # lambda is used for an anonymous callback function
 

3083dc33dbcceb59be90ced121fd41013c74e143
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2021-02-14 23:03:02 +0000

    Use threading to only enable write_button when there's text in text_box

--- Activity Log/qt.py
+++ Activity Log/qt.py
@@ -4,11 +4,15 @@ from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtWidgets import QMainWindow, QApplication, QVBoxLayout, QHBoxLayout, QWidget
 import sys
 import library
+import threading
 
 
 class ActivityLoggerGUI(QMainWindow):
     def __init__(self):
         super(ActivityLoggerGUI, self).__init__()
+
+        # A boolean to see if the window exists. Used to close properly
+        self.exists = True
 
         self.setWindowTitle('Activity Logger')
         # This 550 just makes the window taller to make the text box bigger
@@ -42,7 +46,7 @@ class ActivityLoggerGUI(QMainWindow):
 
         self.exit_button = QtWidgets.QPushButton(self)
         self.exit_button.setText('Exit')
-        self.exit_button.clicked.connect(self.close)
+        self.exit_button.clicked.connect(self.close_properly)
 
         # ===== Arrange all widgets properly
 
@@ -53,6 +57,10 @@ class ActivityLoggerGUI(QMainWindow):
         central_widget = QWidget()
         central_widget.setLayout(self.vbox)
         self.setCentralWidget(central_widget)
+
+        # Start a thread to check the text box and enable the write button if it's got text in it
+        self.button_enable_thread = threading.Thread(target=self.button_enable_loop)
+        self.button_enable_thread.start()
 
     def arrange_widgets(self):
         self.vbox.addWidget(self.info)
@@ -73,6 +81,20 @@ class ActivityLoggerGUI(QMainWindow):
         self.entry_text = self.text_box.toPlainText()
         library.write_entry(self.entry_text)
         self.text_box.setText('')
+
+    def check_text_box(self):
+        if self.text_box.toPlainText() != '':
+            self.write_button.setEnabled(True)
+        else:
+            self.write_button.setEnabled(False)
+
+    def button_enable_loop(self):
+        while self.exists:
+            self.check_text_box()
+
+    def close_properly(self):
+        self.close()
+        self.exists = False
 
 
 def show_window():

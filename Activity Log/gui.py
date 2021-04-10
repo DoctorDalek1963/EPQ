994bdbda042e92c3efda3958f2ebbbb7dff4c430
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2021-04-10 13:02:25 +0100

    Add comma to make text look nicer

--- Activity Log/gui.py
+++ Activity Log/gui.py
@@ -49,7 +49,7 @@ class ActivityLoggerGUI(QMainWindow):
         self._info = QtWidgets.QLabel(self)
         self._info.setText('This text box supports markdown formatting. For instance, you can do *italics*, **bold text**, \n'
                            '`inline code`, [a link](https://google.com), ![an image stored in a folder](image_folder/example.png),\n'
-                           '![an image stored online](https://link.to/image.png)etc.')
+                           '![an image stored online](https://link.to/image.png), etc.')
         self._info.setAlignment(QtCore.Qt.AlignCenter)
 
         self._link = QtWidgets.QLabel(self)
